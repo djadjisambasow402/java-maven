15167aef57678d5607e0399d5c217a96d88c1c98
Djadji Samba Sow <120143228+djadjisambasow402@users.noreply.github.com>
2023-11-27 13:07:13 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,14 @@
 def buildJar() {
     echo "building the application..."
-    powershell 'mvn package'
+    sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        powershell 'docker build -t domoda/awsapp:v1 .'
-        powershell "echo $PASS | docker login -u $USER --password-stdin"
-        powershell 'docker push domoda/awsapp:v1'
+        sh 'docker build -t domoda/awsapp:v1 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push domoda/awsapp:v1'
     }
 } 
 
